07b130d331e355b3712b4ccacbf03365e8bf4d22
Jesse Andrews <anotherjesse@gmail.com>
2024-07-10 14:50:55 -0700

    switch to replicate for generating images

---
+++ README.md
@@ -0,0 +1,15 @@
+high priority:
+
+- [ ] allow prompter to set seed
+- [ ] play sound effect when running a new prompt
+- [ ] record of prompts and outputs
+- [ ] empty text input after image loads
+- [ ] determine correct model (currently SD3)
+
+medium priority:
+- [ ] prompts longer than input don't display correctly clipped
+- [ ] some protection for running new prompts?
+
+low priority:
+- [ ] allow multiple viewers to share remote view (use durable objects?)
+- [ ] allow prompter to choose model (sd3 vs other fast-ish models?)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import index from "./index.html";
+import Replicate from "replicate";
 
 export default {
 	async fetch(request, env, ctx) {
@@ -6,17 +7,36 @@ export default {
 
 		if (url.pathname === '/api/generate-image' && request.method === 'POST') {
 			try {
-				const { prompt } = await request.json();
+				const replicate = new Replicate({
+					auth: env.REPLICATE_API_TOKEN
+				});
 
-				// Simulate image generation based on prompt (replace with actual image generation logic)
-				await new Promise(resolve => setTimeout(resolve, 3000)); // Simulate processing time
-				const size = 1024; // Random size between 500 and 999
-				const imageUrl = `https://picsum.photos/${size}?random=${encodeURIComponent(prompt)}`;
+				const { prompt, seed } = await request.json();
+
+				const input = {
+					cfg: 4.5,
+					seed,
+					prompt,
+					aspect_ratio: "1:1",
+					output_format: "webp",
+					output_quality: 79,
+					negative_prompt: ""
+				};
+
+				const output = await replicate.run("stability-ai/stable-diffusion-3", { input });
+
+				let imageUrl = output[0];
+
+				// // Simulate image generation based on prompt (replace with actual image generation logic)
+				// await new Promise(resolve => setTimeout(resolve, 3000)); // Simulate processing time
+				// const size = 1024; // Random size between 500 and 999
+				// const imageUrl = `https://picsum.photos/${size}?random=${encodeURIComponent(prompt)}`;
 
 				return new Response(JSON.stringify({ imageUrl }), {
 					headers: { 'Content-Type': 'application/json' }
 				});
 			} catch (error) {
+				console.log(error)
 				return new Response(JSON.stringify({ error: 'Invalid request' }), {
 					status: 400,
 					headers: { 'Content-Type': 'application/json' }
